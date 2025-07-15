e94def32f006c2ecabcf04fc31103e63c0641303
lidongjie <lidongjie@eolink.com>
2025-07-15 16:17:54 +0800

    refactor: 简化响应工具类中的成功和错误返回格式

--- services/common/utils/response_utils.py
+++ services/common/utils/response_utils.py
@@ -1,17 +1,13 @@
 class ResponseUtils:
-    
+
     @staticmethod
     def success(data=None, message="success", code=200):
-        return {
-            "code": code,
-            "message": message,
-            "data": data
-        }
+        return {"code": code, "message": message, "data": data}
+
+    @staticmethod
+    def success_page(data=None, message="success", code=200, page_num=0, page_size=0, total=0):
+        return {"code": code, "message": message, "data": data, "pagination": {"page": page_num, "page_size": page_size, "total": total}}
 
     @staticmethod
     def error(message="error", code=1, data=None):
-        return {
-            "code": code,
-            "message": message,
-            "data": data
-        }
+        return {"code": code, "message": message, "data": data}
