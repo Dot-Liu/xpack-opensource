1fc2ea92ada1de7041ebdd0aa33691e0c47735da
lidongjie <lidongjie@eolink.com>
2025-07-17 19:18:24 +0800

    feat: 修改响应格式，统一返回结构中的成功与错误字段，增强一致性

--- services/common/utils/response_utils.py
+++ services/common/utils/response_utils.py
@@ -2,11 +2,12 @@ class ResponseUtils:
 
     @staticmethod
     def success(data=None, message="success", code=200):
-        return {"code": str(code), "error_message": message, "data": data}
+        return {"success": True, "code": str(code), "error_message": message, "data": data}
 
     @staticmethod
     def success_page(data=None, message="success", code=200, page_num=0, page_size=0, total=0):
         return {
+            "success": True,
             "code": str(code),
             "error_message": message,
             "data": data,
@@ -18,4 +19,4 @@ class ResponseUtils:
         if error_msg and isinstance(error_msg, dict):
             code = error_msg.get("code", code)
             message = error_msg.get("message", message)
-        return {"code": str(code), "error_message": message, "data": data}
+        return {"success": False, "code": str(code), "error_message": message, "data": data}
